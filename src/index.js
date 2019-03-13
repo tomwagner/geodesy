6ae1e982d2fba7a4fcc7a9140b3a88af910ceb7e
Tom Wagner <tomas.wagner@gmail.com>
2019-03-13 13:47:55 +0100

    Npm build

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import LatLonEllipsoidal from './latlon-ellipsoidal'
+import LatLonEllipsoidal_Datum from './latlon-ellipsoidal-datum'
+import LatLonEllipsoidal_ReferenceFrame from './latlon-ellipsoidal-referenceframe'
+import LatLonEllipsoidal_Vincenty from './latlon-ellipsoidal-vincenty'
+import LatLonSpherical from './latlon-spherical'
+import LatLonNvectorSpherical from './latlon-nvector-spherical'
+import LatLon_NvectorEllipsoidal from './latlon-nvector-ellipsoidal'
+import Vector3d          from './vector3d';
+import Utm               from './utm';
+import Mgrs              from './mgrs';
+import OsGridRef         from './osgridref';
+import Dms from './dms.js';
+
+export {
+  Dms,
+  LatLon_NvectorEllipsoidal,
+  LatLonEllipsoidal,
+  LatLonEllipsoidal_Datum,
+  LatLonEllipsoidal_ReferenceFrame,
+  LatLonEllipsoidal_Vincenty,
+  LatLonNvectorSpherical,
+  LatLonSpherical,
+  Mgrs,
+  OsGridRef,
+  Utm,
+  Vector3d
+}
